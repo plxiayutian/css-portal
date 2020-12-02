357f4475bb7a7c0b8ac2bc3690776c5d5d15c6c0
gaotengfei <d_chocolate@163.com>
2020-12-02 17:28:12 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,59 @@
+// 页面展示端路由
+// require.ensure 是 Webpack 的特殊语法，用来设置 code-split point
+
+// 展示页面
+const Index = resolve => {
+	require.ensure(['../views/portal/index.vue'], () => {
+		resolve(require('../views/portal/index.vue'))
+	})
+}
+// 普通用户编辑页面
+const IndexEdit = resolve => {
+	require.ensure(['../views/portal/indexEdit.vue'], () => {
+		resolve(require('../views/portal/indexEdit.vue'))
+	})
+}
+// 管理员编辑页面
+const AdminEdit = resolve => {
+	require.ensure(['../views/portal/adminEdit.vue'], () => {
+		resolve(require('../views/portal/adminEdit.vue'))
+	})
+}
+// 错误页面
+const ErrPage = resolve => {
+	require.ensure(['../views/portal/errPage.vue'], () => {
+		resolve(require('../views/portal/errPage.vue'))
+	})
+}
+export default [{
+		path: '/',
+		redirect: '/index',
+		component: Index
+	},
+	{
+		path: '/index',
+		name: 'index',
+		component: Index
+	},
+	{
+		path: '/indexEdit',
+		name: 'indexEdit',
+		component: IndexEdit
+	},
+	{
+		path: '/adminEdit',
+		name: 'adminEdit',
+		component: AdminEdit
+	},
+	{
+		path: '/errPage',
+		name: 'errPage',
+		component: ErrPage
+	}
+	// {
+	// 	path: '*',
+	// 	component: Index
+	// 	// ES7的懒加载，webpack2官网推荐使用,需要配合babel的syntax-dynamic- 
+	// 	// component: () => import('../pages/index/index.vue')
+	// }
+]
---
+++ src/views/portal/errPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="errPage">
+    <img v-if="img500" src="../../assets/images/err/500.jpg" style="width: 100%;" />
+    <img v-if="img404" src="../../assets/images/err/404.jpg" style="width: 100%;" />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "errPage",
+    data(){
+      return {
+        img500:false,
+        img404:false
+      }
+    },
+    mounted() {
+      console.log(this.$route.query)
+      if(this.$route.query.status == "500"){
+        this.img500 = true
+      }else if(this.$route.query.status == "404"){
+        this.img404 = true
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  .errPage{
+    width: 100%;
+    height: 100%;
+  }
+ 
+</style>
